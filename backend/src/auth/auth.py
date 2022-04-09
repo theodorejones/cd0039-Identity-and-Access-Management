d411f87951524f803fa2ebbb3c22f24a2fb5c488
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-04-09 14:05:28 -0400

    Edit Auth0 info

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-sbt6d75y.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'https://localhost:5000'
 
 ## AuthError Exception
 '''
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'manage', // the auth0 domain prefix
+    audience: 'https://localhost:5000', // the audience set for the auth0 app
+    clientId: '6vKC5RkovM9XROX9HeywPwRQ07QQv9B4', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
