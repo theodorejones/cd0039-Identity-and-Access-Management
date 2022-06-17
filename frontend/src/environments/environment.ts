db865f506a241a4a0580e449c7fba4a4ac202a33
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-06-17 09:53:54 -0400

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
-  production: false,
+  production: false,  
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'manage', // the auth0 domain prefix
     audience: 'https://localhost:5000', // the audience set for the auth0 app
-    clientId: '6vKC5RkovM9XROX9HeywPwRQ07QQv9B4', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    clientId: 'biZ6CFIirItzhiSdKbRLXk98ZmushNhI', // the client id generated for the auth0 app
+    callbackURL: 'https://localhost:8080/login-results', // the base url of the running ionic application. 
   }
 };
