744a00bdaa77b2fabf1a3e243717c4eb1723026d
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-06-23 09:18:09 -0400

    Update Auth0 and Postman configs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,  
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'manage', // the auth0 domain prefix
+    url: 'dev-sbt6d75y', // the auth0 domain prefix
     audience: 'https://localhost:5000', // the audience set for the auth0 app
     clientId: 'biZ6CFIirItzhiSdKbRLXk98ZmushNhI', // the client id generated for the auth0 app
     callbackURL: 'https://localhost:8080/login-results', // the base url of the running ionic application. 
